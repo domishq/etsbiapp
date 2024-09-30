5ed718b4f7c9a1cc19f80cb1e4b110b31f86de10
Dominik Tomic <tomic.contact@gmail.com>
2024-09-30 11:24:46 +0200

    update db

--- etsbiapp.sql
+++ etsbiapp.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2023 at 09:05 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Sep 30, 2024 at 11:24 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,14 +37,15 @@ CREATE TABLE `ucenici` (
   `city` varchar(50) NOT NULL,
   `razred` int(1) NOT NULL,
   `odjeljenje` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `ucenici`
 --
 
 INSERT INTO `ucenici` (`id`, `name`, `surname`, `parentsName`, `dateOfBirth`, `placeOfBirth`, `city`, `razred`, `odjeljenje`) VALUES
-(1, 'Dominik', 'Tomic', 'Mario', '2006-06-29', 'Bihać', 'Bihać', 3, 1);
+(1, 'Dominik', 'Tomic', 'Mario', '2006-06-29', 'Bihać', 'Bihać', 3, 1),
+(2, 'Larisa', 'Dalic', 'Ekrem', '2006-02-19', 'Bihać', 'Bihać', 4, 2);
 
 -- --------------------------------------------------------
 
@@ -58,7 +59,7 @@ CREATE TABLE `users` (
   `surname` varchar(20) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
@@ -78,7 +79,7 @@ CREATE TABLE `uvjerenja` (
   `uvjerenje_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `uvjerenja`
@@ -86,6 +87,27 @@ CREATE TABLE `uvjerenja` (
 
 INSERT INTO `uvjerenja` (`uvjerenje_id`, `user_id`, `timestamp`) VALUES
 (1, 1, '2023-12-22 08:05:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uvjerenja_svrhe`
+--
+
+CREATE TABLE `uvjerenja_svrhe` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `text` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `uvjerenja_svrhe`
+--
+
+INSERT INTO `uvjerenja_svrhe` (`id`, `name`, `text`) VALUES
+(1, 'zdravstveno osiguranje', 'regulisanja zdravstvenog osiguranja'),
+(2, 'troskovi prijevoza', 'troskova prijevoza'),
+(3, 'povrat poreza', 'povrata poreza');
 
 --
 -- Indexes for dumped tables
@@ -110,6 +132,13 @@ ALTER TABLE `uvjerenja`
   ADD PRIMARY KEY (`uvjerenje_id`);
 
 --
+-- Indexes for table `uvjerenja_svrhe`
+--
+ALTER TABLE `uvjerenja_svrhe`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -117,7 +146,7 @@ ALTER TABLE `uvjerenja`
 -- AUTO_INCREMENT for table `ucenici`
 --
 ALTER TABLE `ucenici`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -130,6 +159,12 @@ ALTER TABLE `users`
 --
 ALTER TABLE `uvjerenja`
   MODIFY `uvjerenje_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `uvjerenja_svrhe`
+--
+ALTER TABLE `uvjerenja_svrhe`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
